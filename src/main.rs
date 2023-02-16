825fa4afae9a2b2983ac0028e8ff9eb01a7d14f4
Albert Bos <albert@myndocs.nl>
2023-02-16 11:03:52 +0100

    first commit

---
+++ src/main.rs
@@ -0,0 +1,165 @@
+mod config;
+mod gateway;
+mod ui;
+
+use crate::config::read_from_file;
+use crate::gateway::{OpenApiEntry, Route};
+use axum::http::{Method, StatusCode};
+use axum::response::IntoResponse;
+use axum::{
+    extract::Extension,
+    http::{uri::Uri, Request, Response},
+    routing::get,
+    AddExtensionLayer, Router,
+};
+use hyper::{client::HttpConnector, Body};
+use hyper_rustls::HttpsConnector;
+use serde_json::{Map, Value};
+use std::sync::Arc;
+use std::{convert::TryFrom, net::SocketAddr};
+use std::collections::HashMap;
+use axum::http::header::CONTENT_TYPE;
+use crate::gateway::openapi::build_from_json;
+use crate::ui::{SwaggerUiConfig, Url};
+
+type Client = hyper::client::Client<HttpsConnector<HttpConnector>, Body>;
+
+const CONFIG_FILE: &str = "openapi-gateway-config.toml";
+
+#[tokio::main]
+async fn main() {
+    let https = hyper_rustls::HttpsConnectorBuilder::new()
+        .with_webpki_roots()
+        .https_only()
+        .enable_http1()
+        .build();
+
+    let client: Client = hyper::Client::builder().build(https);
+
+    let config = read_from_file(CONFIG_FILE).unwrap();
+
+    let mut entries = vec![];
+    for url in config.openapi_urls.iter() {
+        let response = client
+            .request(Request::get(&url.url).body(Body::empty()).unwrap())
+            .await
+            .unwrap();
+
+        let bytes = hyper::body::to_bytes(response.into_body()).await.unwrap();
+
+        entries.push(build_from_json(&url.url.as_str(), url.name.as_str(), &bytes));
+    }
+
+    let app = Router::new()
+        .route(
+            "/*key",
+            get(handler)
+                .post(handler)
+                .put(handler)
+                .options(handler)
+                .head(handler),
+        )
+        .layer(AddExtensionLayer::new(client))
+        .layer(AddExtensionLayer::new(Arc::new(entries)));
+
+    let addr = SocketAddr::from(([0, 0, 0, 0], 4000));
+    println!("reverse proxy listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn handler(
+    Extension(entries): Extension<Arc<Vec<OpenApiEntry>>>,
+    Extension(client): Extension<Client>,
+    mut req: Request<Body>,
+) -> Response<Body> {
+
+    let path = req.uri().path();
+    let path_query = req
+        .uri()
+        .path_and_query()
+        .map(|v| v.as_str())
+        .unwrap_or(path);
+
+    if req.method() == Method::GET && path.starts_with("/docs") {
+        let mut file = &path[5..].trim_start_matches("/");
+
+        if file.starts_with(".") || file.starts_with("..") {
+            return Response::builder()
+                .status(StatusCode::NOT_FOUND)
+                .body(Body::empty())
+                .unwrap();
+        }
+
+        if file.is_empty() {
+            file = &"index.html";
+        }
+
+        println!("Static file {file}");
+        let mut builder =  Response::builder()
+            .status(StatusCode::OK);
+
+
+        if file.ends_with(".json") {
+            builder = builder.header(CONTENT_TYPE, "application/json");
+        }
+
+
+        let bytes = if file == &"swagger-config.json" {
+            let config = SwaggerUiConfig {
+                urls: entries.iter()
+                    .map(|entry| {
+                        Url {
+                            name: entry.name.clone(),
+                            url: entry.uri.to_string()
+                        }
+                    })
+                    .collect()
+            };
+
+            serde_json::to_vec(&config).unwrap()
+        } else {
+            std::fs::read(format!("swagger-ui/{}", file)).unwrap()
+        };
+        return builder.body(
+                Body::from(bytes)
+            )
+            .unwrap();
+    }
+
+    println!("{path_query}");
+
+    let entry = entries
+        .iter()
+        .filter(|val| {
+            let routes_strs: Vec<_> = val.routes.iter().map(|val| val.uri.clone()).collect();
+
+            routes_strs.contains(&path_query.to_string())
+        })
+        .last();
+
+    if let Some(entry) = entry {
+        println!("Entry found");
+        let entry_uri: &Uri = &entry.uri;
+
+        let uri = format!(
+            "{}://{}{}",
+            entry_uri.scheme_str().unwrap(),
+            entry_uri.host().unwrap(),
+            path_query
+        );
+
+        println!("Forward to {uri}");
+
+        *req.uri_mut() = Uri::try_from(uri).unwrap();
+
+        client.request(req).await.unwrap()
+    } else {
+        Response::builder()
+            .status(StatusCode::NOT_FOUND)
+            .body(Body::empty())
+            .unwrap()
+    }
+}
